f437f8687e73101fa9b569dc7acb46911a3e803c
ZuzkaLysakova <43932311+ZuzkaLysakova@users.noreply.github.com>
2019-02-18 19:12:13 +0100

    Update Program.cs

--- cv1csharp/cv1csharp/Program.cs
+++ cv1csharp/cv1csharp/Program.cs
@@ -6,10 +6,67 @@ using System.Threading.Tasks;
 
 namespace cv1csharp
 {
+    class Address
+    {
+        public string Street;
+        public string Number;
+        public string City;
+        public string Postcode;
+    }
+    class Person
+    {
+        public string Name;
+        public string Surname;
+        public Address Address;
+    }
+
     class Program
     {
+        const uint DEFAULT_COUNT = 100;
+        static uint GetCount(string[] args)
+        {
+            //uint count = 100;
+            if (args.Length == 0) return DEFAULT_COUNT;
+
+            if (uint.TryParse(args[0], out uint temp)) return temp;
+            //uint.TryParse(args[0], out count); //out predavani pomoci reference
+            return DEFAULT_COUNT;
+        }
         static void Main(string[] args)
         {
+            Console.Write("Hello world!");
+            //Console.ReadLine();
+            Console.WriteLine("You sure?"); //zkracene cw tab
+            uint count = GetCount(args);
+            //uint count = uint.Parse(args[0]);
+            //string grettings = "Hello worl";//string s maly s je alias
+            ////String grettings2 = new string("Hello world");
+
+
+            //for (uint i = 0; i < count; i++)
+            //{
+            //    grettings += "again ";
+            //}
+
+            //grettings = grettings.Insert(grettings.Length -1, "!");
+            //Console.WriteLine(grettings);
+
+            if (count != 0)
+            {
+                StringBuilder gretingsBuilder = new StringBuilder();
+                gretingsBuilder.Append("Helo world again");
+                for (int i = 1; i < count; i++)
+                {
+                    gretingsBuilder.Append(" again");
+                }
+                gretingsBuilder.Append("!");
+                Console.WriteLine(gretingsBuilder.ToString());
+            }
+            
         }
+        //vyresit aby za poslednim again nebyla mezera
+        //vyresit aby se to zvladlo vyresit velke cislo za kratky cas
+        //ctrl k, d - odsazení
+        
     }
 }
