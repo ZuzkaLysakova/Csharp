bb234727d29dcbb96447bd0918dd9d47b0a9c61f
ZuzkaLysakova <43932311+ZuzkaLysakova@users.noreply.github.com>
2019-03-04 17:41:54 +0100

    save

---
+++ SamplesLibrary/Cviceni03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils;
+
+namespace Cviceni03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomNumberGenerator.Next();
+        }
+    }
+}
