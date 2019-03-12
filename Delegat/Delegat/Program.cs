a8d18456e554c85ae26edec0870915ce910d2a70
ZuzkaLysakova <43932311+ZuzkaLysakova@users.noreply.github.com>
2019-03-12 11:05:55 +0100

    cv3

---
+++ Delegat/Delegat/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegat
+{
+
+    delegate int PorovnavacStudentu(Student stud1, Student stud2);
+    public enum Fakulta { FES, FF, FEI, FCHT };
+
+    class Student
+    {
+        public string Jmeno { get; set; }
+        public int Cislo { get; set; }
+
+        public Fakulta Fakulta { get; set; }
+
+
+    }
+    class Studenti
+    {
+        private Student[] studenti;
+        int pocet;
+        public Studenti()
+        {
+            studenti = new Student[10];
+            pocet = 0;
+        }
+
+        public void VypisStudenty()
+        {
+            for (int i = 0; i < studenti.Length; i++)
+            {
+                Console.WriteLine(studenti[i].Jmeno, studenti[i].Cislo, studenti[i].Fakulta);
+            }
+
+        }
+        public void SeradStudenty(PorovnavacStudentu porovnavacStudentu)
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Studenti studenti = new Studenti();
+
+            int prikaz = 0;
+            switch (prikaz)
+            {
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                case 1:
+
+                    break;
+                case 2:
+                    studenti.VypisStudenty();
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+            }
+        }
+    }
+}
