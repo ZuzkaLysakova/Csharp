ff6327677d4889c352f6e3375c42b832839c9f57
ZuzkaLysakova <43932311+ZuzkaLysakova@users.noreply.github.com>
2019-03-12 12:12:19 +0100

    Update Delegat/Delegat/Program.cs

--- Delegat/Delegat/Program.cs
+++ Delegat/Delegat/Program.cs
@@ -17,12 +17,13 @@ namespace Delegat
 
         public Fakulta Fakulta { get; set; }
 
-
     }
+
     class Studenti
     {
         private Student[] studenti;
         int pocet = 0;
+
         public Studenti()
         {
             studenti = new Student[10];
@@ -43,7 +44,10 @@ namespace Delegat
             {
                 for (int j = 0; j < studenti.Length - 1; j++)
                 {
-                    
+                    if (porovnavacStudentu(studenti[i], studenti[j+1]) > 0)
+                    {
+                        
+                    }
                 }
             }
         }
@@ -74,8 +78,36 @@ namespace Delegat
 
     class Program
     {
+        static public int PorovnejId(Student st1, Student st2)
+        {
+            if (st1.Cislo < st2.Cislo)
+            {
+                return -1;
+            }
+            else if (st1.Cislo > st2.Cislo)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        static public int PorovnejJmena(Student st1, Student st2)
+        {
+            return st1.Jmeno.CompareTo(st2.Jmeno);
+        }
+
+        static public int PorovnejFakulty(Student st1, Student st2)
+        {
+            return st1.Fakulta.CompareTo(st2.Fakulta);
+        }
+
         static void Main(string[] args)
         {
+
+            
             Studenti studenti = new Studenti();
             Console.WriteLine("Možnosti:");
             Console.WriteLine("0- ukonči program");
@@ -85,6 +117,7 @@ namespace Delegat
             Console.WriteLine("4- seřaď podle jména");
             Console.WriteLine("5- seřaď podle fakulty");
             int prikaz = 0;
+            PorovnavacStudentu porov = null;
             switch (prikaz)
             {
                 case 0:
@@ -101,13 +134,16 @@ namespace Delegat
                     studenti.VypisStudenty();
                     break;
                 case 3:
-
+                    porov = PorovnejId;
+                    studenti.SeradStudentypodleId(porov);
                     break;
                 case 4:
-
+                    porov = PorovnejJmena;
+                    studenti.SeradStudentypodleJmena(porov);
                     break;
                 case 5:
-
+                    porov = PorovnejFakulty;
+                    studenti.SeradStudentypodleFakulty(porov);
                     break;
             }
         }
