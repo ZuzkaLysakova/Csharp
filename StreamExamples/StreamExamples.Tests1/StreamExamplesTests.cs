30c4344c6de6f7b5057dc1d507bda6f2ccb8fcad
ZuzkaLysakova <43932311+ZuzkaLysakova@users.noreply.github.com>
2019-04-05 18:57:51 +0200

    streamExample

---
+++ StreamExamples/StreamExamples.Tests1/StreamExamplesTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using StreamExamples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamExamples.Tests
+{
+    [TestFixture]
+    public class StreamExamplesTests
+    {
+        [Test]
+        public void FileExampleTest()
+        {
+
+            StreamExamples.FileExample();
+            //Assert.Fail();
+        }
+    }
+}
---
+++ StreamExamples/StreamExamples/StreamExamples.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamExamples
+{
+    public class StreamExamples
+    {
+
+        public static void FileExample()
+        {
+            const string testPath01 = "d:\\test.txt";
+            if (!File.Exists(testPath01))
+            {
+
+                FileStream test1 = File.Create(testPath01);
+            }
+            const string testPath02 = "d:\\testWithBuffer.txt";
+            if (!File.Exists(testPath02))
+            {
+                FileStream test2 = File.Create(testPath02, sizeof(int) * 100, FileOptions.Encrypted | FileOptions.Asynchronous);
+                test2.Flush();
+                try
+                {
+                    StreamWriter sw = File.CreateText(testPath02);
+                    //using (StreamWriter sw = File.CreateText(testPath02))
+                    {
+
+                        try
+                        {
+                            sw.Write("test text");
+                            throw new ArgumentException();
+                        }
+                        catch (IndexOutOfRangeException e)
+                        {
+
+                        }
+                    }
+                }catch(Exception e)
+                {
+
+                }
+                using (StreamWriter sw = File.CreateText(testPath02))
+                {
+
+                }
+
+
+            }
+            FileInfo fileInfoTest1 = new FileInfo(testPath01);
+            if (fileInfoTest1.Exists) 
+            {
+                var sw = fileInfoTest1.Create();
+                sw.Close();
+            }
+
+            DriveInfo di = new DriveInfo("C");
+            var freeSpace = di.TotalFreeSpace;
+
+        }
+
+    }
+}
